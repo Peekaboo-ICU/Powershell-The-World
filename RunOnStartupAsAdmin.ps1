bab008da60254f6a0de6e2ad0950c236fc856889
Peekaboo-ICU <134564332+Peekaboo-ICU@users.noreply.github.com>
2023-07-04 14:23:56 +1000

    Update RunOnStartupAsAdmin.ps1

--- RunOnStartupAsAdmin.ps1
+++ RunOnStartupAsAdmin.ps1
@@ -6,10 +6,8 @@ $shortcut = $shell.CreateShortcut($shortcutPath)
 $shortcut.TargetPath = $scriptPath
 $shortcut.Save()
 
-# Get the shortcut object using the Windows Script Host Shell object
-$shortcut = $shell.CreateShortcut($shortcutPath)
-
-# Update the shortcut properties to run as administrator
+# Elevate the shortcut to run as administrator
 $shortcut.WindowStyle = 1
 $shortcut.Save()
+
 
